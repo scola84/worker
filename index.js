d8aad79f82b30e5bf1c9eb3a2367bd965093edec
M.C. van den Boogaart <scola@kvdb.name>
2017-11-27 00:29:23 +0100

    Add Unifier

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import Manager from './src/manager';
 import Router from './src/router';
 import Streamer from './src/streamer';
 import Timer from './src/timer';
+import Unifier from './src/unifier';
 import Worker from './src/worker';
 
 export {
@@ -13,5 +14,6 @@ export {
   Router,
   Streamer,
   Timer,
+  Unifier,
   Worker
 };
---
+++ src/unifier.js
@@ -0,0 +1,15 @@
+import Worker from './worker';
+
+export default class Unifier extends Worker {
+  act(box, data, callback) {
+    box.unify.count = (box.unify.count || 0) + 1;
+
+    if (box.unify.count === box.unify.total) {
+      this.pass(box, data, callback);
+    }
+  }
+
+  decide(box) {
+    return typeof box.unify !== 'undefined';
+  }
+}
