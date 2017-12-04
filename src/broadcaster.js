39a186278293d480dd51cca88ed6154a0161b3e2
M.C. van den Boogaart <scola@kvdb.name>
2017-12-04 11:54:53 +0100

    Add unify option to broadcaster

--- src/broadcaster.js
+++ src/broadcaster.js
@@ -3,7 +3,14 @@ import Worker from './worker';
 export default class Broadcaster extends Worker {
   constructor(methods) {
     super(methods);
+
+    this._unify = null;
     this._workers = [];
+  }
+
+  unify(value) {
+    this._unify = value;
+    return this;
   }
 
   connect(worker) {
@@ -12,6 +19,10 @@ export default class Broadcaster extends Worker {
   }
 
   pass(box, data, callback) {
+    if (this._unify !== null && typeof box.unify === 'undefined') {
+      box.unify = { total: this._unify };
+    }
+
     for (let i = 0; i < this._workers.length; i += 1) {
       this._workers[i].handle(box, data, callback);
     }
