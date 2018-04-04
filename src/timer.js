d53d761e4473637b19b4a2ff8f21b4bb15e04af4
M.C. van den Boogaart <scola@kvdb.name>
2018-04-04 16:29:25 +0200

    Allow timer to handle immediately

--- src/timer.js
+++ src/timer.js
@@ -1,7 +1,11 @@
 import Worker from './worker';
 
 export default class Timer extends Worker {
-  start(interval) {
+  start(interval, immediate = false) {
     setInterval(() => this.handle(), interval);
+
+    if (immediate === true) {
+      this.handle();
+    }
   }
 }
