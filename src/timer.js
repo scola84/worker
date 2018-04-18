8014d15c46d9275ab69e8fc28ca909df34c7446c
M.C. van den Boogaart <scola@kvdb.name>
2018-04-18 14:04:43 +0200

    Add schedule to timer

--- src/timer.js
+++ src/timer.js
@@ -1,10 +1,44 @@
+import { scheduleJob } from 'node-schedule';
 import Worker from './worker';
 
 export default class Timer extends Worker {
-  start(interval, immediate = false) {
-    setInterval(() => this.handle(), interval);
+  constructor(options = {}) {
+    super(options);
 
-    if (immediate === true) {
+    this._immediate = null;
+    this._interval = null;
+    this._schedule = null;
+
+    this.setImmediate(options.immediate);
+    this.setInterval(options.interval);
+    this.setSchedule(options.schedule);
+  }
+
+  setImmediate(value = null) {
+    this._immediate = value;
+    return this;
+  }
+
+  setInterval(value = null) {
+    this._interval = value;
+    return this;
+  }
+
+  setSchedule(value = null) {
+    this._schedule = value;
+    return this;
+  }
+
+  start() {
+    if (this._schedule !== null) {
+      scheduleJob(this._schedule, () => this.handle());
+    }
+
+    if (this._interval !== null) {
+      setInterval(() => this.handle(), this._interval);
+    }
+
+    if (this._immediate === true) {
       this.handle();
     }
   }
