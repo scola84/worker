df1186f0d826a76c9f1759da948d48f31e9fac6d
M.C. van den Boogaart <scola@kvdb.name>
2018-12-19 16:18:21 +0100

    Add global queue factory method

--- src/queuer.js
+++ src/queuer.js
@@ -1,16 +1,22 @@
-import queue from 'async/queue';
+import asyncQueue from 'async/queue';
 import Worker from './worker';
 
+const queues = {};
+
 export default class Queuer extends Worker {
+  static createQueue(concurrency, name) {
+    return createQueue(concurrency, name);
+  }
+
   constructor(options = {}) {
     super(options);
 
     this._concurrency = null;
-    this._delay = null;
+    this._name = null;
     this._queue = null;
 
     this.setConcurrency(options.concurrency);
-    this.setDelay(options.delay);
+    this.setName(options.name);
   }
 
   setConcurrency(value = 1) {
@@ -18,17 +24,26 @@ export default class Queuer extends Worker {
     return this;
   }
 
-  setDelay(value = null) {
-    this._delay = value;
+  setName(value = null) {
+    this._name = value;
     return this;
   }
 
-  act(box, data, callback) {
+  act(box, data) {
     if (this._queue === null) {
       this._createQueue();
     }
 
-    this._queue.push({ box, data, callback });
+    this._queue.push((callback) => {
+      this.pass(box, data, (...args) => {
+        callback(...args);
+
+        if (this._log === 'queue') {
+          console.log('queuer (%s): fn=callback, length=%s, running=%s',
+            this._id, this._queue.length(), this._queue.running());
+        }
+      });
+    });
 
     if (this._log === 'queue') {
       console.log('queuer (%s): fn=act, length=%s, running=%s',
@@ -37,25 +52,24 @@ export default class Queuer extends Worker {
   }
 
   _createQueue() {
-    this._queue = queue(({ box, data }, callback) => {
-      if (this._delay === null) {
-        return this._execute(box, data, callback);
-      }
+    this._queue = this._name === null ?
+      createQueue(this._concurrency) :
+      queues[this._name];
+  }
+}
 
-      return setTimeout(() => {
-        this._execute(box, data, callback);
-      }, this._delay);
-    }, this._concurrency);
+function createQueue(concurrency, name) {
+  const queue = asyncQueue((fn, callback) => {
+    fn(callback);
+  }, concurrency);
+
+  if (typeof name !== 'undefined') {
+    if (typeof queues[name] !== 'undefined') {
+      throw new Error(`Queue already defined (name=${name})`);
+    }
+
+    queues[name] = queue;
   }
 
-  _execute(box, data, callback) {
-    this.pass(box, data, (...args) => {
-      callback(...args);
-
-      if (this._log === 'queue') {
-        console.log('queuer (%s): fn=callback, length=%s, running=%s',
-          this._id, this._queue.length(), this._queue.running());
-      }
-    });
-  }
+  return queue;
 }
