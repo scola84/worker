7acc584716af0851e4fe37e4dc4715cd268c7836
M.C. van den Boogaart <scola@kvdb.name>
2019-08-27 12:47:50 +0200

    Fix rollup export

--- src/helper/rollup/index.js
+++ src/helper/rollup/index.js
@@ -1,3 +1,11 @@
-export * from './external'
-export * from './globals'
-export * from './plugins'
+import { banner } from './banner'
+import { external } from './external'
+import { globals } from './globals'
+import { plugins } from './plugins'
+
+export const rollup = {
+  banner,
+  external,
+  globals,
+  plugins
+}
